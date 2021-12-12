bea5255575831bb10657d80404ca234f500e836a
Đức Nguyễn <nguyenanhduc10101010@gmail.com>
2021-12-12 22:09:34 +0700

    ducna

---
+++ Publisher.java
@@ -0,0 +1,59 @@
+package pubsubbro;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+import action.Action;
+import action.JoinAction.ClientType;
+import action.PublishDataAction;
+import action.ReplyJoinAction;
+import action.UnsubcriptionAction;
+
+public class Publisher extends Client{
+	
+	public Publisher(ClientType clientType, String location) {
+		super(clientType, location);		
+	}	
+	
+	public void process() {
+		
+		Action act = receiveAction();			
+		if (!(act instanceof ReplyJoinAction)) {
+			return;
+		}
+		ReplyJoinAction reply = (ReplyJoinAction) act;
+		
+		if (!(reply.getState() == 0)) {
+			System.out.println(Config.messageToClient.get(reply.getState()));
+		} else {		
+			System.out.println(Config.messageToClient.get(reply.getState()));
+			while (true) {
+				try {
+					String data = "hello";
+					PublishDataAction data_update = new PublishDataAction(data, this.location);		
+					out.writeObject(data_update);
+					out.flush();
+					
+					System.out.println("Send completed");
+					Thread.sleep(Config.SLEEP_TIME);
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InterruptedException  e) {
+				}
+			}
+ 		}	
+	}	
+	
+	public void quit() {
+		String msg = "quit action from pub " + getLocation();	
+		sendAction(new UnsubcriptionAction(msg));
+		close();	
+	}
+
+	
+	
+}
---
+++ pubClientHandler.java
@@ -0,0 +1,51 @@
+package pubsubbro;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Set;
+
+import action.Action;
+import action.PublishDataAction;
+import action.UnsubcriptionAction;
+import action.JoinAction.ClientType;
+
+public class pubClientHandler extends ClientHandler {
+
+	private Set<subClientHandler> sub;
+	
+	
+	public pubClientHandler(long id, Socket socket, ObjectInputStream in, ObjectOutputStream out, Set<subClientHandler> setSub, String location) {
+		super(id, socket, in, out, location);
+		// TODO Auto-generated constructor stub
+		this.sub = setSub;	
+	}
+	
+	public void setSub(Set<subClientHandler> sub) {
+		this.sub = sub;
+	}
+	
+	public Set<subClientHandler> getSub() {
+		return this.sub;
+	}
+		
+	
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Action act = (Action) this.in.readObject();				
+					// get data to other sub
+				for (subClientHandler s : sub) {
+					s.sendAction(act);									
+				}  		
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch(ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
